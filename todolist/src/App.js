788d71d68e89aed92cd98f32b1445fe7a944e4c4
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-02-27 23:10:09 -0300

    fim todolist
    Neste commit finalizei a todolist

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -83,3 +83,47 @@ function List (props) {
 }
 
 export default List;
+
+// src/Todo.js
+return (
+  <div className="todo-wrapper">
+    <h1>ToDo List</h1>
+    <form >
+      <input type="text" onChange={handleInputText} value={text} />
+      <button type="submit" onClick={addItem} >Adiciona</button>
+    </form>
+  
+    <List itemsList={itemsList} /> {/* <--------- passando o `itemList` como props para o componente */}
+  </div>
+);
+
+import React, { useState } from 'react';
+
+function Form(props) {
+  const [task, setTask] = useState("");
+
+  function handleChangeInput(event) {
+    let inputTask = event.target.value;
+    
+    setTask(inputTask);
+  }
+
+  function handleAddItemToList(event) {
+    event.preventDefault();
+    
+    if(task) {
+      props.handleAddItemToList(task) // <-----------
+      
+      setTask("");
+    }
+  }
+  
+  return (
+    <form >
+      <input type="text" onChange={handleChangeInput} value={task} />
+      <button type="submit" onClick={handleAddItemToList}>Adiciona</button>
+    </form>
+  )
+}
+
+export default Form;
